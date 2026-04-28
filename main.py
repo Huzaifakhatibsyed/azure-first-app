28e98e0aace3fa3c7e40889ad06841c04e14983a
HUZAIFA KS <khatibsyedhuzaifa@gmail.com>
2026-04-28 15:06:42 +0530

    added about route

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-from flask import Flask
-import os
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "My First Azure App 🚀"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+@app.route('/about')
+def about():
+    return "This is my Azure deployed app"
