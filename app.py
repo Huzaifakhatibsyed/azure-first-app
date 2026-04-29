509daaac26730e698698c202845efa8b0746aaa2
HUZAIFA KS <khatibsyedhuzaifa@gmail.com>
2026-04-29 18:46:26 +0530

    fix the error

--- app.py
+++ app.py
@@ -1,3 +1,27 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "I am learning Azure 🚀"
+
+
 @app.route('/about')
 def about():
-    return "This is my Azure deployed app"
+    return "About page"
+
+
+@app.route('/user')
+def user():
+    return jsonify({
+        "name": "Huzaifa",
+        "status": "learning azure"
+    })
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
